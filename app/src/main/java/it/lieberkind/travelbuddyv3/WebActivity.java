8359141c6f5719802a0e17ff981e8101deaf2f6f
Tomas Lieberkind <t.lieberkind@gmail.com>
2015-02-25 19:47:59 +0100

    Removed empty line

--- app/src/main/java/it/lieberkind/travelbuddyv3/WebActivity.java
+++ app/src/main/java/it/lieberkind/travelbuddyv3/WebActivity.java
@@ -22,7 +22,6 @@ public class WebActivity extends Activity {
         browser.loadUrl("www.dsb.dk");
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
